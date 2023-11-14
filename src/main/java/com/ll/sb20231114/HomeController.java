7a87d4bb3d43d7184a5f8ee603d359fde9cbdf69
itonse <govl3dnjs@gmail.com>
2023-11-14 11:15:58 +0900

    feat : 계산기 기능 6~7, 8~13
    [6~7]
    - 함수의 return값을 스프링이 String으로 번역 후에 브라우저로 전송한다.
    - 객체의 문자열화는 잭슨이 json 방식으로 변환
    
    [8~13]
    - json

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -1,9 +1,15 @@
 package com.ll.sb20231114;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -52,5 +58,87 @@ public class HomeController {
     String showCalc5(@RequestParam(defaultValue = "-") String a,
                      @RequestParam(defaultValue = "-") String b) {
         return "계산 결과 : %s".formatted(a + b);
+    }
+
+    @GetMapping("/calc6")    // http://localhost:8020/calc6?a=5&b=6
+    @ResponseBody
+    int showCalc6(int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/calc7")    // http://localhost:8020/calc7?a=5&b=10
+    @ResponseBody
+    boolean showCalc7(int a, int b) {
+        return a > b;
+    }
+
+    @GetMapping("/calc8")    // http://localhost:8020/calc8?name=홍길동&age=20
+    @ResponseBody
+    Person showCalc8(String name, int age) {
+        return new Person(name, age);
+    }
+
+    @GetMapping("/calc9")    //  http://localhost:8020/calc9?name=홍길동&age=20
+    @ResponseBody
+    Person2 showCalc9(String name, int age
+    ) {
+        return new Person2(name, age);
+    }
+
+    @GetMapping("/calc10")    // http://localhost:8020/calc8?name=홍길동&age=20
+    @ResponseBody
+    Map<String, Object> showCalc10(String name, int age) {
+        Map<String, Object> personMap = Map.of(    // 출력 순서는 랜덤
+                "name", name,
+                "age", age
+        );
+
+        return personMap;
+    }
+
+    @GetMapping("/calc11")    // http://localhost:8020/calc11
+    @ResponseBody
+    List<Integer> showCalc11() {
+        List<Integer> nums = new ArrayList<>() {{
+            add(10);
+            add(100);
+        }};
+
+        return nums;
+    }
+
+    @GetMapping("/calc12")    // http://localhost:8020/calc12
+    @ResponseBody
+    int[] showCalc12() {
+        int[] nums = new int[]{10, 100, 500};
+
+        return nums;
+    }
+
+    @GetMapping("/calc13")    // http://localhost:8020/calc13?name=홍길동&age=20
+    @ResponseBody
+    List<Person2> showCalc13(String name, int age) {
+        List<Person2> persons = new ArrayList<>() {{    // 익명클래스 이용
+            add(new Person2(name, age));
+            add(new Person2(name + "!", age + 1));
+            add(new Person2(name + "!!", age + 2));
+
+        }};
+
+        return persons;
+    }
+
+    @AllArgsConstructor
+    class Person {
+        public String name;
+        public int age;
+    }
+
+    @AllArgsConstructor
+    class Person2 {
+        @Getter
+        private String name;
+        @Getter
+        private int age;
     }
 }
