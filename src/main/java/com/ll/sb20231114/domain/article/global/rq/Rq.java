da596e97a284607a1858bde44e21da66d329ad7a
itonse <govl3dnjs@gmail.com>
2023-11-15 15:46:48 +0900

    feat : (심화)step34 Rq, @RequestScope

--- src/main/java/com/ll/sb20231114/domain/article/global/rq/Rq.java
+++ src/main/java/com/ll/sb20231114/domain/article/global/rq/Rq.java
@@ -3,7 +3,9 @@ package com.ll.sb20231114.domain.article.global.rq;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
+@RequestScope    // 객체의 생명주기 설정 (HTTP 요청마다 객체의 인스턴스가 새롭게 생성되고, 끝날 때 제거된다)
 @Component
 public class Rq {
     private final HttpServletRequest req;
