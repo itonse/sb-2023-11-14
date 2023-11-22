bd4ffa5a4b3a07c4958d37ed7f59e28a14ece70c
itonse <govl3dnjs@gmail.com>
2023-11-22 10:28:19 +0900

    feat : step52 예외처리를 write.html에서가 아닌 Controller에서 한다
    테스트를 위해서 자바스크립트 유효성 검사를 잠시 끈다. rsData 에 F-1, F-2 등의 코드를 담아서 예외상황이 발생했다는 메세지를 예쁘게 보여준다.

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -1,6 +1,5 @@
 package com.ll.sb20231114.domain.article.article.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.domain.article.global.rq.Rq;
@@ -8,7 +7,6 @@ import com.ll.sb20231114.domain.article.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +31,20 @@ public class ArticleController {
             String title,
             String body
     ) {
+        if (title == null || title.trim().length() == 0) {
+            return new RsData<>(
+                    "F-1",
+                    "제목을 입력해주세요."
+            );
+        }
+
+        if (body == null || body.trim().length() == 0) {
+            return new RsData<>(
+                    "F-2",
+                    "내용을 입력해주세요."
+            );
+        }
+
         Article article = articleService.write(title, body);
 
         return new RsData<>(     // <> 안에 타입 생략 가능
@@ -40,30 +52,6 @@ public class ArticleController {
                 "%d번 게시물이 작성되었습니다.".formatted(article.getId()),   // 성공 메시지
                 article    // 작성된 게시물 객체
         );
-    }
-
-    @PostMapping("/article/write2")
-    @SneakyThrows
-    void write2(    // 위의 write 메서드의 전통적인 서블릿 방식
-            HttpServletRequest req,
-            HttpServletResponse resp
-    ) {
-        String title = req.getParameter("title");
-        String body = req.getParameter("body");
-
-        Article article = articleService.write(title, body);
-
-        RsData<Article> rs = new RsData<>(
-                "S-1",
-                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
-                article
-        );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().println(objectMapper.writeValueAsString(rs));
     }
 
     @GetMapping("/article/getLastArticle")
--- src/main/java/com/ll/sb20231114/domain/article/global/rsData/RsData.java
+++ src/main/java/com/ll/sb20231114/domain/article/global/rsData/RsData.java
@@ -2,11 +2,13 @@ package com.ll.sb20231114.domain.article.global.rsData;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
+@RequiredArgsConstructor   // 이것에 의해 final이 붙은 resultCode, msg를 받는 생성자가 만들어짐
 @Getter
 public class RsData<T> {     // API 응답을 나타내는 제네릭 클래스
-    private String resultCode;
-    private String msg;
+    private final String resultCode;
+    private final String msg;
     private T data;     // data의 타입을 지정하지 않아, 유연하게 값을 받을 수 있다.
 }
